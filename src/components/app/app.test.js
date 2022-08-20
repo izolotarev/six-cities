f8f3241df10b0896dc9badca07ac3797c2d1c6a7
Igor Zolotarev <izolotarev@hotmail.com>
2022-08-20 15:06:12 +0300

    refactor: app test

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -4,10 +4,8 @@ import {Router} from 'react-router-dom';
 import {createMemoryHistory} from 'history';
 import * as redux from 'react-redux';
 import configureStore from 'redux-mock-store';
-import {AuthorizationStatus, AppRoute} from '../../const/const';
+import {AuthorizationStatus} from '../../const/const';
 import App from './app';
-import { makeFakeOffer } from '../../utils/mocks';
-import { adaptToClient } from '../../utils/utils';
 
 const mockStore = configureStore({});
 describe(`Test routing`, () => {
@@ -18,7 +16,7 @@ describe(`Test routing`, () => {
     const history = createMemoryHistory();
     const store = mockStore({
       OFFERS: {
-        selectedCity: "Paris",
+        selectedCity: 'Paris',
         offers: [],
         isDataLoaded: true,
       },
